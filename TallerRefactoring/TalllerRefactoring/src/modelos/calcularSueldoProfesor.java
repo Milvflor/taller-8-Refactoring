cb6ccb26a9849aac769db2f9b0d1480d24b05fc3
Milca Valdez Flores <milvflor@espol.edu.ec>
2021-08-12 20:53:34 -0500

    eliminar clase calcularSueldoProfesor

--- TallerRefactoring/TalllerRefactoring/src/modelos/calcularSueldoProfesor.java
+++
@@ -1,9 +0,0 @@
-package modelos;
-
-public class calcularSueldoProfesor {
-    
-    public double calcularSueldo(Profesor prof){
-        return prof.getInfo().getBonoFijo()*600 + prof.getInfo().getBonoFijo();
-        
-    }  
-}
